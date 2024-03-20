cc109c355dbd187daefba92212b2442c919452a1
cs-patzer <cs-patzer@protonmail.com>
2024-03-20 15:29:48 +0100

    Add file.rs and rank.rs

---
+++ src/file.rs
@@ -0,0 +1,14 @@
+/// A file on the chessboard.
+enum File {
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3,
+    E = 4,
+    F = 5,
+    G = 6,
+    H = 7,
+}
+
+/// The number of files on a chessboard.
+pub const NUM_FILES: u8 = 8;
---
+++ src/rank.rs
@@ -0,0 +1,14 @@
+/// A rank on the chessboard.
+enum Rank {
+    First = 0,
+    Second = 1,
+    Third = 2,
+    Fourth = 3,
+    Fifth = 4,
+    Sixth = 5,
+    Seventh = 6,
+    Eighth = 7,
+}
+
+/// The number of ranks on a chessboard.
+pub const NUM_RANKS: u8 = 8;
