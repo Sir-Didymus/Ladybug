fdadfc8725229e6a48be1bc2748f3cf8d671aa74
cs-patzer <cs-patzer@protonmail.com>
2024-03-21 21:31:27 +0100

    Add position.rs

---
+++ src/position.rs
@@ -0,0 +1,40 @@
+use crate::bitboard::Bitboard;
+use crate::file::File;
+
+/// This struct represents a chess position. 
+/// It contains 12 bitboards, one for each peace for each color.
+/// It also contains information on whether en passant is possible.
+pub struct Position {
+    /// White's king bitboard
+    white_king_bb: Bitboard,
+    /// Black's king bitboard
+    black_king_bb: Bitboard,
+
+    /// White's queen bitboard
+    white_queen_bb: Bitboard,
+    /// Black's queen bitboard
+    black_queen_bb: Bitboard,
+
+    /// White's rook bitboard
+    white_rook_bb: Bitboard,
+    /// Black's rook bitboard
+    black_rook_bb: Bitboard,
+
+    /// White's knight bitboard
+    white_knight_bb: Bitboard,
+    /// Black's knight bitboard
+    black_knight_bb: Bitboard,
+
+    /// White's bishop bitboard
+    white_bishop_bb: Bitboard,
+    /// Black's bishop bitboard
+    black_bishop_bb: Bitboard,
+
+    /// White's pawn bitboard
+    white_pawn_bb: Bitboard,
+    /// Black's pawn bitboard
+    black_pawn_bb: Bitboard,
+
+    /// If en passant is possible, this Option contains the target file for the en passant move
+    en_passant: Option<File>,
+}
