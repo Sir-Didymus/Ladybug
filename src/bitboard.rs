f43770a483c96d70a52332b6894596f3f4ed111c
cs-patzer <cs-patzer@protonmail.com>
2024-03-21 19:55:15 +0100

    Rename get_square and set_square to get_bit and set_bit

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -20,13 +20,13 @@ impl Bitboard {
         Self { value }
     }
 
-    /// Returns true if the given square is set to 1, false if not.
-    pub fn get_square(&self, square: Square) -> bool {
+    /// Returns true if the bit at the specified square is set to 1, false if not.
+    pub fn get_bit(&self, square: Square) -> bool {
         (1 << square.index) & self.value > 0
     }
 
-    /// Sets the bitboard at the given square to 1
-    pub fn set_square(&mut self, square: Square) {
+    /// Sets the bit at the specified square to 1
+    pub fn set_bit(&mut self, square: Square) {
         self.value ^= 1 << square.index;
     }
 }
@@ -38,7 +38,7 @@ impl Display for Bitboard {
         for rank in (0..NUM_RANKS).rev() {
             output += format!("{}  ", rank + 1).as_str();
             for file in 0..NUM_FILES {
-                if self.get_square(Square::from_file_rank(File::from_index(file), Rank::from_index(rank))) {
+                if self.get_bit(Square::from_file_rank(File::from_index(file), Rank::from_index(rank))) {
                     output += "X  ";
                 } else {
                     output += ".  ";
@@ -82,10 +82,10 @@ mod tests {
     }
 
     #[test]
-    fn set_square_sets_correct_square() {
+    fn set_bit_sets_bit_at_correct_square() {
         for i in 0..64 {
             let mut bitboard = Bitboard::new(0);
-            bitboard.set_square(Square::new(i));
+            bitboard.set_bit(Square::new(i));
             assert!((1 << i) & bitboard.value > 0); // test that square is set - I avoid using get_square here so the tests are independent of each other
             for j in 0..64 { // test that all other squares are not set
                 if i != j {
@@ -96,78 +96,78 @@ mod tests {
     }
 
     #[test]
-    fn get_square_returns_correct_state_of_square() {
+    fn get_bit_returns_bit_at_correct_square() {
         // test every square, initializing the bitboard using hexadecimal, so it is shorter to write
-        assert!(Bitboard::new(0x1).get_square(Square::from_file_rank(File::A, Rank::First)));
-        assert!(Bitboard::new(0x2).get_square(Square::from_file_rank(File::B, Rank::First)));
-        assert!(Bitboard::new(0x4).get_square(Square::from_file_rank(File::C, Rank::First)));
-        assert!(Bitboard::new(0x8).get_square(Square::from_file_rank(File::D, Rank::First)));
-        assert!(Bitboard::new(0x10).get_square(Square::from_file_rank(File::E, Rank::First)));
-        assert!(Bitboard::new(0x20).get_square(Square::from_file_rank(File::F, Rank::First)));
-        assert!(Bitboard::new(0x40).get_square(Square::from_file_rank(File::G, Rank::First)));
-        assert!(Bitboard::new(0x80).get_square(Square::from_file_rank(File::H, Rank::First)));
+        assert!(Bitboard::new(0x1).get_bit(Square::from_file_rank(File::A, Rank::First)));
+        assert!(Bitboard::new(0x2).get_bit(Square::from_file_rank(File::B, Rank::First)));
+        assert!(Bitboard::new(0x4).get_bit(Square::from_file_rank(File::C, Rank::First)));
+        assert!(Bitboard::new(0x8).get_bit(Square::from_file_rank(File::D, Rank::First)));
+        assert!(Bitboard::new(0x10).get_bit(Square::from_file_rank(File::E, Rank::First)));
+        assert!(Bitboard::new(0x20).get_bit(Square::from_file_rank(File::F, Rank::First)));
+        assert!(Bitboard::new(0x40).get_bit(Square::from_file_rank(File::G, Rank::First)));
+        assert!(Bitboard::new(0x80).get_bit(Square::from_file_rank(File::H, Rank::First)));
 
-        assert!(Bitboard::new(0x100).get_square(Square::from_file_rank(File::A, Rank::Second)));
-        assert!(Bitboard::new(0x200).get_square(Square::from_file_rank(File::B, Rank::Second)));
-        assert!(Bitboard::new(0x400).get_square(Square::from_file_rank(File::C, Rank::Second)));
-        assert!(Bitboard::new(0x800).get_square(Square::from_file_rank(File::D, Rank::Second)));
-        assert!(Bitboard::new(0x1000).get_square(Square::from_file_rank(File::E, Rank::Second)));
-        assert!(Bitboard::new(0x2000).get_square(Square::from_file_rank(File::F, Rank::Second)));
-        assert!(Bitboard::new(0x4000).get_square(Square::from_file_rank(File::G, Rank::Second)));
-        assert!(Bitboard::new(0x8000).get_square(Square::from_file_rank(File::H, Rank::Second)));
+        assert!(Bitboard::new(0x100).get_bit(Square::from_file_rank(File::A, Rank::Second)));
+        assert!(Bitboard::new(0x200).get_bit(Square::from_file_rank(File::B, Rank::Second)));
+        assert!(Bitboard::new(0x400).get_bit(Square::from_file_rank(File::C, Rank::Second)));
+        assert!(Bitboard::new(0x800).get_bit(Square::from_file_rank(File::D, Rank::Second)));
+        assert!(Bitboard::new(0x1000).get_bit(Square::from_file_rank(File::E, Rank::Second)));
+        assert!(Bitboard::new(0x2000).get_bit(Square::from_file_rank(File::F, Rank::Second)));
+        assert!(Bitboard::new(0x4000).get_bit(Square::from_file_rank(File::G, Rank::Second)));
+        assert!(Bitboard::new(0x8000).get_bit(Square::from_file_rank(File::H, Rank::Second)));
 
-        assert!(Bitboard::new(0x10000).get_square(Square::from_file_rank(File::A, Rank::Third)));
-        assert!(Bitboard::new(0x20000).get_square(Square::from_file_rank(File::B, Rank::Third)));
-        assert!(Bitboard::new(0x40000).get_square(Square::from_file_rank(File::C, Rank::Third)));
-        assert!(Bitboard::new(0x80000).get_square(Square::from_file_rank(File::D, Rank::Third)));
-        assert!(Bitboard::new(0x100000).get_square(Square::from_file_rank(File::E, Rank::Third)));
-        assert!(Bitboard::new(0x200000).get_square(Square::from_file_rank(File::F, Rank::Third)));
-        assert!(Bitboard::new(0x400000).get_square(Square::from_file_rank(File::G, Rank::Third)));
-        assert!(Bitboard::new(0x800000).get_square(Square::from_file_rank(File::H, Rank::Third)));
+        assert!(Bitboard::new(0x10000).get_bit(Square::from_file_rank(File::A, Rank::Third)));
+        assert!(Bitboard::new(0x20000).get_bit(Square::from_file_rank(File::B, Rank::Third)));
+        assert!(Bitboard::new(0x40000).get_bit(Square::from_file_rank(File::C, Rank::Third)));
+        assert!(Bitboard::new(0x80000).get_bit(Square::from_file_rank(File::D, Rank::Third)));
+        assert!(Bitboard::new(0x100000).get_bit(Square::from_file_rank(File::E, Rank::Third)));
+        assert!(Bitboard::new(0x200000).get_bit(Square::from_file_rank(File::F, Rank::Third)));
+        assert!(Bitboard::new(0x400000).get_bit(Square::from_file_rank(File::G, Rank::Third)));
+        assert!(Bitboard::new(0x800000).get_bit(Square::from_file_rank(File::H, Rank::Third)));
 
-        assert!(Bitboard::new(0x1000000).get_square(Square::from_file_rank(File::A, Rank::Fourth)));
-        assert!(Bitboard::new(0x2000000).get_square(Square::from_file_rank(File::B, Rank::Fourth)));
-        assert!(Bitboard::new(0x4000000).get_square(Square::from_file_rank(File::C, Rank::Fourth)));
-        assert!(Bitboard::new(0x8000000).get_square(Square::from_file_rank(File::D, Rank::Fourth)));
-        assert!(Bitboard::new(0x10000000).get_square(Square::from_file_rank(File::E, Rank::Fourth)));
-        assert!(Bitboard::new(0x20000000).get_square(Square::from_file_rank(File::F, Rank::Fourth)));
-        assert!(Bitboard::new(0x40000000).get_square(Square::from_file_rank(File::G, Rank::Fourth)));
-        assert!(Bitboard::new(0x80000000).get_square(Square::from_file_rank(File::H, Rank::Fourth)));
+        assert!(Bitboard::new(0x1000000).get_bit(Square::from_file_rank(File::A, Rank::Fourth)));
+        assert!(Bitboard::new(0x2000000).get_bit(Square::from_file_rank(File::B, Rank::Fourth)));
+        assert!(Bitboard::new(0x4000000).get_bit(Square::from_file_rank(File::C, Rank::Fourth)));
+        assert!(Bitboard::new(0x8000000).get_bit(Square::from_file_rank(File::D, Rank::Fourth)));
+        assert!(Bitboard::new(0x10000000).get_bit(Square::from_file_rank(File::E, Rank::Fourth)));
+        assert!(Bitboard::new(0x20000000).get_bit(Square::from_file_rank(File::F, Rank::Fourth)));
+        assert!(Bitboard::new(0x40000000).get_bit(Square::from_file_rank(File::G, Rank::Fourth)));
+        assert!(Bitboard::new(0x80000000).get_bit(Square::from_file_rank(File::H, Rank::Fourth)));
 
-        assert!(Bitboard::new(0x100000000).get_square(Square::from_file_rank(File::A, Rank::Fifth)));
-        assert!(Bitboard::new(0x200000000).get_square(Square::from_file_rank(File::B, Rank::Fifth)));
-        assert!(Bitboard::new(0x400000000).get_square(Square::from_file_rank(File::C, Rank::Fifth)));
-        assert!(Bitboard::new(0x800000000).get_square(Square::from_file_rank(File::D, Rank::Fifth)));
-        assert!(Bitboard::new(0x1000000000).get_square(Square::from_file_rank(File::E, Rank::Fifth)));
-        assert!(Bitboard::new(0x2000000000).get_square(Square::from_file_rank(File::F, Rank::Fifth)));
-        assert!(Bitboard::new(0x4000000000).get_square(Square::from_file_rank(File::G, Rank::Fifth)));
-        assert!(Bitboard::new(0x8000000000).get_square(Square::from_file_rank(File::H, Rank::Fifth)));
+        assert!(Bitboard::new(0x100000000).get_bit(Square::from_file_rank(File::A, Rank::Fifth)));
+        assert!(Bitboard::new(0x200000000).get_bit(Square::from_file_rank(File::B, Rank::Fifth)));
+        assert!(Bitboard::new(0x400000000).get_bit(Square::from_file_rank(File::C, Rank::Fifth)));
+        assert!(Bitboard::new(0x800000000).get_bit(Square::from_file_rank(File::D, Rank::Fifth)));
+        assert!(Bitboard::new(0x1000000000).get_bit(Square::from_file_rank(File::E, Rank::Fifth)));
+        assert!(Bitboard::new(0x2000000000).get_bit(Square::from_file_rank(File::F, Rank::Fifth)));
+        assert!(Bitboard::new(0x4000000000).get_bit(Square::from_file_rank(File::G, Rank::Fifth)));
+        assert!(Bitboard::new(0x8000000000).get_bit(Square::from_file_rank(File::H, Rank::Fifth)));
 
-        assert!(Bitboard::new(0x10000000000).get_square(Square::from_file_rank(File::A, Rank::Sixth)));
-        assert!(Bitboard::new(0x20000000000).get_square(Square::from_file_rank(File::B, Rank::Sixth)));
-        assert!(Bitboard::new(0x40000000000).get_square(Square::from_file_rank(File::C, Rank::Sixth)));
-        assert!(Bitboard::new(0x80000000000).get_square(Square::from_file_rank(File::D, Rank::Sixth)));
-        assert!(Bitboard::new(0x100000000000).get_square(Square::from_file_rank(File::E, Rank::Sixth)));
-        assert!(Bitboard::new(0x200000000000).get_square(Square::from_file_rank(File::F, Rank::Sixth)));
-        assert!(Bitboard::new(0x400000000000).get_square(Square::from_file_rank(File::G, Rank::Sixth)));
-        assert!(Bitboard::new(0x800000000000).get_square(Square::from_file_rank(File::H, Rank::Sixth)));
+        assert!(Bitboard::new(0x10000000000).get_bit(Square::from_file_rank(File::A, Rank::Sixth)));
+        assert!(Bitboard::new(0x20000000000).get_bit(Square::from_file_rank(File::B, Rank::Sixth)));
+        assert!(Bitboard::new(0x40000000000).get_bit(Square::from_file_rank(File::C, Rank::Sixth)));
+        assert!(Bitboard::new(0x80000000000).get_bit(Square::from_file_rank(File::D, Rank::Sixth)));
+        assert!(Bitboard::new(0x100000000000).get_bit(Square::from_file_rank(File::E, Rank::Sixth)));
+        assert!(Bitboard::new(0x200000000000).get_bit(Square::from_file_rank(File::F, Rank::Sixth)));
+        assert!(Bitboard::new(0x400000000000).get_bit(Square::from_file_rank(File::G, Rank::Sixth)));
+        assert!(Bitboard::new(0x800000000000).get_bit(Square::from_file_rank(File::H, Rank::Sixth)));
 
-        assert!(Bitboard::new(0x1000000000000).get_square(Square::from_file_rank(File::A, Rank::Seventh)));
-        assert!(Bitboard::new(0x2000000000000).get_square(Square::from_file_rank(File::B, Rank::Seventh)));
-        assert!(Bitboard::new(0x4000000000000).get_square(Square::from_file_rank(File::C, Rank::Seventh)));
-        assert!(Bitboard::new(0x8000000000000).get_square(Square::from_file_rank(File::D, Rank::Seventh)));
-        assert!(Bitboard::new(0x10000000000000).get_square(Square::from_file_rank(File::E, Rank::Seventh)));
-        assert!(Bitboard::new(0x20000000000000).get_square(Square::from_file_rank(File::F, Rank::Seventh)));
-        assert!(Bitboard::new(0x40000000000000).get_square(Square::from_file_rank(File::G, Rank::Seventh)));
-        assert!(Bitboard::new(0x80000000000000).get_square(Square::from_file_rank(File::H, Rank::Seventh)));
+        assert!(Bitboard::new(0x1000000000000).get_bit(Square::from_file_rank(File::A, Rank::Seventh)));
+        assert!(Bitboard::new(0x2000000000000).get_bit(Square::from_file_rank(File::B, Rank::Seventh)));
+        assert!(Bitboard::new(0x4000000000000).get_bit(Square::from_file_rank(File::C, Rank::Seventh)));
+        assert!(Bitboard::new(0x8000000000000).get_bit(Square::from_file_rank(File::D, Rank::Seventh)));
+        assert!(Bitboard::new(0x10000000000000).get_bit(Square::from_file_rank(File::E, Rank::Seventh)));
+        assert!(Bitboard::new(0x20000000000000).get_bit(Square::from_file_rank(File::F, Rank::Seventh)));
+        assert!(Bitboard::new(0x40000000000000).get_bit(Square::from_file_rank(File::G, Rank::Seventh)));
+        assert!(Bitboard::new(0x80000000000000).get_bit(Square::from_file_rank(File::H, Rank::Seventh)));
 
-        assert!(Bitboard::new(0x100000000000000).get_square(Square::from_file_rank(File::A, Rank::Eighth)));
-        assert!(Bitboard::new(0x200000000000000).get_square(Square::from_file_rank(File::B, Rank::Eighth)));
-        assert!(Bitboard::new(0x400000000000000).get_square(Square::from_file_rank(File::C, Rank::Eighth)));
-        assert!(Bitboard::new(0x800000000000000).get_square(Square::from_file_rank(File::D, Rank::Eighth)));
-        assert!(Bitboard::new(0x1000000000000000).get_square(Square::from_file_rank(File::E, Rank::Eighth)));
-        assert!(Bitboard::new(0x2000000000000000).get_square(Square::from_file_rank(File::F, Rank::Eighth)));
-        assert!(Bitboard::new(0x4000000000000000).get_square(Square::from_file_rank(File::G, Rank::Eighth)));
-        assert!(Bitboard::new(0x8000000000000000).get_square(Square::from_file_rank(File::H, Rank::Eighth)));
+        assert!(Bitboard::new(0x100000000000000).get_bit(Square::from_file_rank(File::A, Rank::Eighth)));
+        assert!(Bitboard::new(0x200000000000000).get_bit(Square::from_file_rank(File::B, Rank::Eighth)));
+        assert!(Bitboard::new(0x400000000000000).get_bit(Square::from_file_rank(File::C, Rank::Eighth)));
+        assert!(Bitboard::new(0x800000000000000).get_bit(Square::from_file_rank(File::D, Rank::Eighth)));
+        assert!(Bitboard::new(0x1000000000000000).get_bit(Square::from_file_rank(File::E, Rank::Eighth)));
+        assert!(Bitboard::new(0x2000000000000000).get_bit(Square::from_file_rank(File::F, Rank::Eighth)));
+        assert!(Bitboard::new(0x4000000000000000).get_bit(Square::from_file_rank(File::G, Rank::Eighth)));
+        assert!(Bitboard::new(0x8000000000000000).get_bit(Square::from_file_rank(File::H, Rank::Eighth)));
     }
 }
