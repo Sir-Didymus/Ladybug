3a50e0d5d0f6a7cc5d24c21f121aa24715caa846
cs-patzer <cs-patzer@protonmail.com>
2024-03-23 00:58:04 +0100

    Add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod bitboard;
+pub mod square;
+pub mod position;
+pub mod file;
+pub mod rank;
+pub mod color;
+pub mod lookup_table;
+pub mod pawn_attacks;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-use crate::bitboard::Bitboard;
-
-mod bitboard;
-mod square;
-mod position;
-mod file;
-mod rank;
-mod color;
-mod lookup_table;
-mod pawn_attacks;
+use chess_engine::bitboard::Bitboard;
 
 fn main() {
     let bitboard = Bitboard::new(9223372054036742144);
