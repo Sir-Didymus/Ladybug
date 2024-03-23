d78b7d91a6d40039ad738dcde368ea3257f527a3
cs-patzer <cs-patzer@protonmail.com>
2024-03-23 20:20:04 +0100

    Format knight_attacks.rs

--- src/lookup/knight_attacks.rs
+++ src/lookup/knight_attacks.rs
@@ -16,7 +16,7 @@ pub fn generate_knight_attacks() -> [Bitboard; 64] {
 fn get_attack_bb(square: Square) -> Bitboard {
     let mut attack_bb = Bitboard::new(0); // the result attack bitboard
     let knight_bb = Bitboard::from_square(square); // bitboard with the square of the knight set
-    
+
     // North North-East - Offset +17
     if ((knight_bb.value << 17) & NOT_A_FILE.value) > 0 {
         attack_bb.value |= knight_bb.value << 17;
@@ -49,7 +49,7 @@ fn get_attack_bb(square: Square) -> Bitboard {
     if ((knight_bb.value >> 17) & NOT_H_FILE.value) > 0 {
         attack_bb.value |= knight_bb.value >> 17;
     }
-    
+
     attack_bb
 }
 
@@ -66,21 +66,21 @@ mod tests {
             // print attack_bb for debugging purposes
             println!("{attack_bb}");
         }
-        
+
         // If I wanted to test for every square like I did for pawns, I'd basically have to rewrite the get_attack_bb function in here again, just for this test.
         // So I will only test for a few exemplary cases (using hard-coded hex values for the result bitboard). 
         // The correctness of the function can also be verified by checking the console output (see println! above).
         // You can see the knight's attack squares shifting up the bitboard, with over-the-edge captures accounted for by using the file masks.
-        
-        assert_eq!(0x284400442800,get_attack_bb(E4).value);
-        assert_eq!(0x20400,get_attack_bb(A1).value);
-        assert_eq!(0x402000,get_attack_bb(H1).value);
-        assert_eq!(0x4020000000000,get_attack_bb(A8).value);
-        assert_eq!(0x20400000000000,get_attack_bb(H8).value);
-        assert_eq!(0x22140000000000,get_attack_bb(D8).value);
-        assert_eq!(0xa01000,get_attack_bb(G1).value);
-        assert_eq!(0x508000805,get_attack_bb(B3).value);
-        assert_eq!(0x5088008850000000,get_attack_bb(F6).value);
-        assert_eq!(0x1100110a00000000,get_attack_bb(C7).value);
+
+        assert_eq!(0x284400442800, get_attack_bb(E4).value);
+        assert_eq!(0x20400, get_attack_bb(A1).value);
+        assert_eq!(0x402000, get_attack_bb(H1).value);
+        assert_eq!(0x4020000000000, get_attack_bb(A8).value);
+        assert_eq!(0x20400000000000, get_attack_bb(H8).value);
+        assert_eq!(0x22140000000000, get_attack_bb(D8).value);
+        assert_eq!(0xa01000, get_attack_bb(G1).value);
+        assert_eq!(0x508000805, get_attack_bb(B3).value);
+        assert_eq!(0x5088008850000000, get_attack_bb(F6).value);
+        assert_eq!(0x1100110a00000000, get_attack_bb(C7).value);
     }
 }
