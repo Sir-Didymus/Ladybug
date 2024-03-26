6932fac41aad561d2860657082e1526deac7f826
cs-patzer <cs-patzer@protonmail.com>
2024-03-26 21:58:27 +0100

    Add get_pawn_attacks method to LookupTable

--- src/lookup/lookup_table.rs
+++ src/lookup/lookup_table.rs
@@ -1,4 +1,6 @@
 use crate::board::bitboard::Bitboard;
+use crate::board::color::Color;
+use crate::board::square::Square;
 use crate::lookup::{king_attacks, knight_attacks};
 use crate::lookup::pawn_attacks;
 
@@ -28,11 +30,18 @@ impl LookupTable {
         self.knight_attacks = knight_attacks::generate_knight_attacks();
         self.king_attacks = king_attacks::generate_king_attacks();
     }
+
+    /// Returns the attack bitboard for a pawn of the specified color on the specified square.
+    pub fn get_pawn_attacks(&self, square: Square, color: Color) -> Bitboard {
+        self.pawn_attacks[color.to_index() as usize][square.index as usize]
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::board::bitboard::Bitboard;
+    use crate::board::color::Color::{Black, White};
+    use crate::board::square::{B5, C2, F7, H1, H5};
     use crate::lookup::lookup_table::LookupTable;
 
     #[test]
@@ -41,5 +50,23 @@ mod tests {
         assert_eq!([[Bitboard::new(0); 64]; 2], lookup_table.pawn_attacks);
         assert_eq!([Bitboard::new(0); 64], lookup_table.knight_attacks);
         assert_eq!([Bitboard::new(0); 64], lookup_table.king_attacks);
+    }
+
+    #[test]
+    fn get_pawn_attacks_returns_bitboard_with_attacked_bits_set() {
+        let mut lookup_table = LookupTable::default();
+        lookup_table.initialize_tables();
+
+        // Testing the get_pawn_attacks method using fixed hex values for the result bitboard.
+        assert_eq!(0xa0000, lookup_table.get_pawn_attacks(C2, White).value);
+        assert_eq!(0x4000, lookup_table.get_pawn_attacks(H1, White).value);
+        assert_eq!(0x50000000000, lookup_table.get_pawn_attacks(B5, White).value);
+        assert_eq!(0x5000000000000000, lookup_table.get_pawn_attacks(F7, White).value);
+        assert_eq!(0x400000000000, lookup_table.get_pawn_attacks(H5, White).value);
+        assert_eq!(0xa, lookup_table.get_pawn_attacks(C2, Black).value);
+        assert_eq!(0x0, lookup_table.get_pawn_attacks(H1, Black).value);
+        assert_eq!(0x5000000, lookup_table.get_pawn_attacks(B5, Black).value);
+        assert_eq!(0x500000000000, lookup_table.get_pawn_attacks(F7, Black).value);
+        assert_eq!(0x40000000, lookup_table.get_pawn_attacks(H5, Black).value);
     }
 }
