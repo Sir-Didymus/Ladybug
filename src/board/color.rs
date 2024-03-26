60d95ffe97775ec703ae9bf807c8cf8acd3a9c5d
cs-patzer <cs-patzer@protonmail.com>
2024-03-26 20:08:58 +0100

    Add to_index for method for Color

--- src/board/color.rs
+++ src/board/color.rs
@@ -1,4 +1,5 @@
 /// The two colors in the game of chess.
+#[derive(Copy, Clone)]
 pub enum Color {
     White,
     Black,
@@ -6,3 +7,21 @@ pub enum Color {
 
 /// The number of colors.
 pub const NUM_COLORS: u8 = 2;
+
+impl Color {
+    /// Returns the index of the color: 0 for White and 1 for Black.
+    pub fn to_index(&self) -> u8 {
+        *self as u8
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::board::color::Color;
+
+    #[test]
+    fn to_index_returns_correct_index() {
+        assert_eq!(0, Color::White.to_index());
+        assert_eq!(1, Color::Black.to_index());
+    }
+}
