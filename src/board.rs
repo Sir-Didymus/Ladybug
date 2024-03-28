ac0cd79d82fb707a47f24beebd78c59f236d3936
cs-patzer <cs-patzer@protonmail.com>
2024-03-28 13:22:11 +0100

    Implement Default trait for Board struct

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,4 @@
-//! This module deals with basic board representation.
+//! The board module deals with basic board representation.
 //! It contains the important position and bitboard submodules, as well other useful ones such as color, file, rank, and square.
 //! This module is the foundation on which the rest of the engine builds upon.
 
@@ -14,11 +14,37 @@ pub mod piece;
 pub mod position;
 
 /// The board struct holds the current position of the board.
-/// It also keeps track of the half-move (ply) history, the full move count, the number of reversible half-moves (50 move draw),
+/// It also keeps track of the half-move (ply) history, the full move count, the number of reversible half-moves (50 move rule),
 /// and a list of all positions that have been on the board before (threefold repetition).
 #[derive(Copy, Clone)]
 pub struct Board {
-    position: Position,
-    full_move_count: u16,
-    reversible_ply_count: u16,
+    pub position: Position,
+    pub full_move_count: u16,
+    pub reversible_ply_count: u16,
+}
+
+impl Default for Board {
+    /// Default constructor for Board.
+    /// Returns a board with the default position, and all other values set to zero.
+    fn default() -> Self {
+        Self {
+            position: Position::default(),
+            full_move_count: 0,
+            reversible_ply_count: 0,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::board::Board;
+    use crate::board::position::Position;
+
+    #[test]
+    fn default_returns_board_with_default_values() {
+        let board = Board::default();
+        assert_eq!(Position::default(), board.position);
+        assert_eq!(0, board.full_move_count);
+        assert_eq!(0, board.reversible_ply_count);
+    }
 }
