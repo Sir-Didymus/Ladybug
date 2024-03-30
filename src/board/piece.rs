10cef6fc52fa76c5b16b9601268ae3d13e32cae7
cs-patzer <cs-patzer@protonmail.com>
2024-03-30 21:53:03 +0100

    Add to_char method

--- src/board/piece.rs
+++ src/board/piece.rs
@@ -1,3 +1,5 @@
+use crate::board::color::Color;
+
 /// Represents a piece on a chessboard.
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub enum Piece {
@@ -30,10 +32,33 @@ impl Piece {
             _ => unreachable!(),
         }
     }
+    
+    /// Returns a unicode character based on piece type and color.
+    pub fn to_char(&self, color: Color) -> char {
+        match color {
+            Color::White => match self {
+                Piece::Pawn => '\u{2659}',
+                Piece::Knight => '\u{2658}',
+                Piece::Bishop => '\u{2657}',
+                Piece::Rook => '\u{2656}',
+                Piece::Queen => '\u{2655}',
+                Piece::King => '\u{2654}',
+            }
+            Color::Black => match self {
+                Piece::Pawn => '\u{265F}',
+                Piece::Knight => '\u{265E}',
+                Piece::Bishop => '\u{265D}',
+                Piece::Rook => '\u{265C}',
+                Piece::Queen => '\u{265B}',
+                Piece::King => '\u{265A}',
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::board::color::Color::{Black, White};
     use crate::board::piece::Piece;
     use crate::board::piece::Piece::*;
 
@@ -66,5 +91,22 @@ mod tests {
         assert_eq!(Queen, Piece::from_index(10));
         assert_eq!(King, Piece::from_index(11));
         assert_eq!(Pawn, Piece::from_index(12));
+    }
+    
+    #[test]
+    fn to_char_returns_correct_unicode_char() {
+        assert_eq!('\u{2659}', Pawn.to_char(White));
+        assert_eq!('\u{2658}', Knight.to_char(White));
+        assert_eq!('\u{2657}', Bishop.to_char(White));
+        assert_eq!('\u{2656}', Rook.to_char(White));
+        assert_eq!('\u{2655}', Queen.to_char(White));
+        assert_eq!('\u{2654}', King.to_char(White));
+
+        assert_eq!('\u{265F}', Pawn.to_char(Black));
+        assert_eq!('\u{265E}', Knight.to_char(Black));
+        assert_eq!('\u{265D}', Bishop.to_char(Black));
+        assert_eq!('\u{265C}', Rook.to_char(Black));
+        assert_eq!('\u{265B}', Queen.to_char(Black));
+        assert_eq!('\u{265A}', King.to_char(Black));
     }
 }
