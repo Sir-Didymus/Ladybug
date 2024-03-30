7355259633bdeda8dd6e01fefb002468f16d6f1a
cs-patzer <cs-patzer@protonmail.com>
2024-03-30 19:55:06 +0100

    Add from_index in piece.rs

--- src/board/piece.rs
+++ src/board/piece.rs
@@ -1,5 +1,5 @@
 /// Represents a piece on a chessboard.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub enum Piece {
     Pawn = 0,
     Knight = 1,
@@ -17,10 +17,24 @@ impl Piece {
     pub fn to_index(&self) -> u8 {
         *self as u8
     }
+
+    /// Constructs a piece based on the piece's index.
+    pub fn from_index(index: u8) -> Piece {
+        match index % 6 {
+            0 => Piece::Pawn,
+            1 => Piece::Knight,
+            2 => Piece::Bishop,
+            3 => Piece::Rook,
+            4 => Piece::Queen,
+            5 => Piece::King,
+            _ => unreachable!(),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::board::piece::Piece;
     use crate::board::piece::Piece::*;
 
     #[test]
@@ -31,5 +45,26 @@ mod tests {
         assert_eq!(3, Rook.to_index());
         assert_eq!(4, Queen.to_index());
         assert_eq!(5, King.to_index());
+    }
+
+    #[test]
+    fn from_index_with_valid_index_returns_piece() {
+        assert_eq!(Pawn, Piece::from_index(0));
+        assert_eq!(Knight, Piece::from_index(1));
+        assert_eq!(Bishop, Piece::from_index(2));
+        assert_eq!(Rook, Piece::from_index(3));
+        assert_eq!(Queen, Piece::from_index(4));
+        assert_eq!(King, Piece::from_index(5));
+    }
+
+    #[test]
+    fn from_index_with_invalid_index_wraps_around() {
+        assert_eq!(Pawn, Piece::from_index(6));
+        assert_eq!(Knight, Piece::from_index(7));
+        assert_eq!(Bishop, Piece::from_index(8));
+        assert_eq!(Rook, Piece::from_index(9));
+        assert_eq!(Queen, Piece::from_index(10));
+        assert_eq!(King, Piece::from_index(11));
+        assert_eq!(Pawn, Piece::from_index(12));
     }
 }
