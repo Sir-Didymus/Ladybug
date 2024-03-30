c8b90e6e402bd88eda1e254516c250c01f435797
cs-patzer <cs-patzer@protonmail.com>
2024-03-30 19:27:45 +0100

    Add from_index in color.rs

--- src/board/color.rs
+++ src/board/color.rs
@@ -13,6 +13,15 @@ impl Color {
     pub fn to_index(&self) -> u8 {
         *self as u8
     }
+    
+    /// Returns a color based on the color's index.
+    pub fn from_index(index: u8) -> Color {
+        match index % 2 {
+            0 => Color::White,
+            1 => Color::Black,
+            _ => unreachable!(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -23,5 +32,17 @@ mod tests {
     fn to_index_returns_correct_index() {
         assert_eq!(0, Color::White.to_index());
         assert_eq!(1, Color::Black.to_index());
+    }
+
+    #[test]
+    fn from_index_with_valid_index_returns_color() {
+        assert_eq!(Color::White, Color::from_index(0));
+        assert_eq!(Color::Black, Color::from_index(1));
+    }
+    
+    #[test]
+    fn from_index_with_invalid_index_wraps_around() {
+        assert_eq!(Color::White, Color::from_index(2));
+        assert_eq!(Color::Black, Color::from_index(3));
     }
 }
