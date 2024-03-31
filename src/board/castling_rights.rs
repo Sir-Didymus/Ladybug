54e23bb28b35c8028155899a6c79ed816a5d9c08
cs-patzer <cs-patzer@protonmail.com>
2024-03-31 14:23:27 +0200

    Implement Display for CastlingRights

--- src/board/castling_rights.rs
+++ src/board/castling_rights.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 /// Contains information on what (if any) castling rights a player has.
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub enum CastlingRights {
@@ -5,4 +7,24 @@ pub enum CastlingRights {
     KingSide,
     QueenSide,
     Both,
+}
+
+/// Prints the castling rights as text.
+impl Display for CastlingRights {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        match self {
+            CastlingRights::NoRights => write!(f, "NoRights"),
+            CastlingRights::KingSide => write!(f, "KingSide"),
+            CastlingRights::QueenSide => write!(f, "QueenSide"),
+            CastlingRights::Both => write!(f, "Both"),
+        }
+    }
+}
+
+#[test]
+fn castling_rights_formats_correctly() {
+    assert_eq!("NoRights", format!("{}", CastlingRights::NoRights));
+    assert_eq!("KingSide", format!("{}", CastlingRights::KingSide));
+    assert_eq!("QueenSide", format!("{}", CastlingRights::QueenSide));
+    assert_eq!("Both", format!("{}", CastlingRights::Both));
 }
