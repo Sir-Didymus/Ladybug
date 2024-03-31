3825a7cfa0f60cfda1342bc6c9508fee583a6b15
cs-patzer <cs-patzer@protonmail.com>
2024-03-31 17:39:24 +0200

    Implement Display for Position

--- src/board/position.rs
+++ src/board/position.rs
@@ -1,9 +1,12 @@
+use std::fmt::{Display, Formatter};
 use crate::board::bitboard::Bitboard;
 use crate::board::castling_rights::CastlingRights;
 use crate::board::castling_rights::CastlingRights::NoRights;
 use crate::board::color::{Color, NUM_COLORS};
 use crate::board::color::Color::White;
+use crate::board::file::{File, NUM_FILES};
 use crate::board::piece::{NUM_PIECES, Piece};
+use crate::board::rank::{NUM_RANKS, Rank};
 use crate::board::square::Square;
 
 /// This struct uniquely encodes a chess position.
@@ -62,11 +65,43 @@ impl Position  {
     }
 }
 
+/// Prints the position with '.' marking empty squares, capital letters marking white pieces,
+/// and lower case letter marking black pieces.
+/// It also prints the color to move, castling rights, and en passant target square.
+impl Display for Position {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        let mut output: String = String::from("");
+        for rank in (0..NUM_RANKS).rev() {
+            output += format!("{}  ", rank + 1).as_str();
+            for file in 0..NUM_FILES {
+                let piece = self.get_piece(Square::from_file_rank(File::from_index(file), Rank::from_index(rank)));
+                if piece.is_none() {
+                    output += ".  ";
+                } else {
+                    let (piece, color) = piece.unwrap();
+                    output.push(piece.to_char(color));
+                    output += "  ";
+                }
+            }
+            output += "\n";
+        }
+        output += "   a  b  c  d  e  f  g  h\n";
+        output += format!("\nMove: {}", self.color_to_move).as_str();
+        output += format!("\nCastling: {} - {}", self.castling_rights[0],  self.castling_rights[1]).as_str();
+        match self.en_passant {
+            None => output += "\nEn Passant: None\n",
+            Some(square) => output += format!("\nEn Passant: {square}\n").as_str(),
+        }
+        write!(f, "{}", output)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::board::bitboard::Bitboard;
     use crate::board::castling_rights::CastlingRights::NoRights;
     use crate::board::color::Color::{Black, White};
+    use crate::board::fen;
     use crate::board::piece::Piece::{Bishop, King, Knight, Pawn, Queen, Rook};
     use crate::board::position::Position;
     use crate::board::square::{A1, A3, E1, E4, F2, F3, G3, H7, H8};
@@ -128,5 +163,16 @@ mod tests {
         assert_eq!(Some((Knight, Black)), position.get_piece(E4));
         assert_eq!(Some((King, White)), position.get_piece(H8));
         assert_eq!(Some((Bishop, White)), position.get_piece(F2));
+    }
+    
+    #[test]
+    fn position_formats_correctly() {
+        let position = fen::parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap().position;
+        let expected_output = "8  r  n  b  q  k  b  n  r  \n7  p  p  p  p  p  p  p  p  \n6  .  .  .  .  .  .  .  .  \n5  .  .  .  .  .  .  .  .  \n4  .  .  .  .  .  .  .  .  \n3  .  .  .  .  .  .  .  .  \n2  P  P  P  P  P  P  P  P  \n1  R  N  B  Q  K  B  N  R  \n   a  b  c  d  e  f  g  h\n\nMove: White\nCastling: Both - Both\nEn Passant: None\n";
+        assert_eq!(expected_output, format!("{}", position));
+
+        let position = fen::parse_fen("r1bq1rk1/1pp1bppp/p1n2n2/4p3/2PpP1P1/P2P1Q1P/1P1N1P2/R1B1KBNR b KQ g3 0 9").unwrap().position;
+        let expected_output = "8  r  .  b  q  .  r  k  .  \n7  .  p  p  .  b  p  p  p  \n6  p  .  n  .  .  n  .  .  \n5  .  .  .  .  p  .  .  .  \n4  .  .  P  p  P  .  P  .  \n3  P  .  .  P  .  Q  .  P  \n2  .  P  .  N  .  P  .  .  \n1  R  .  B  .  K  B  N  R  \n   a  b  c  d  e  f  g  h\n\nMove: Black\nCastling: Both - NoRights\nEn Passant: g3\n";
+        assert_eq!(expected_output, format!("{}", position));
     }
 }
