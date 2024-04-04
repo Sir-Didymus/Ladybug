98ae4b7728c2de39b9daa6eb2593a7b6d64db403
cs-patzer <cs-patzer@protonmail.com>
2024-04-04 20:22:55 +0200

    Move get_attack_bb functions from move_gen.rs to position.rs

--- src/board/position.rs
+++ src/board/position.rs
@@ -8,6 +8,7 @@ use crate::board::file::{File, NUM_FILES};
 use crate::board::piece::{NUM_PIECES, Piece};
 use crate::board::rank::{NUM_RANKS, Rank};
 use crate::board::square::Square;
+use crate::lookup::LOOKUP_TABLE;
 
 /// This struct uniquely encodes a chess position.
 /// It contains 12 bitboards, one for each piece for each color.
@@ -83,6 +84,51 @@ impl Position  {
         }
         occupancy_bb
     }
+
+    /// Returns the attack bitboard for all pieces of the given color.
+    ///
+    /// For example `get_attack_bb(Color::White)` will return a bitboard with all squares
+    /// set that are attacked by any of White's pieces.
+    pub fn get_attack_bb(&self, color: Color) -> Bitboard {
+        // the result attack_bb
+        let mut attack_bb = Bitboard::new(0);
+        // `or` the attack bitboards for all pieces of the given color
+        for piece_index in 0..NUM_PIECES {
+            attack_bb.value |= self.get_piece_attack_bb(Piece::from_index(piece_index), color).value;
+        }
+        attack_bb
+    }
+
+    /// Returns the attack bitboard for the given type of piece of the given color.
+    ///
+    /// For example `get_attack_bb(Piece::Bishop, Color::White)` will return a bitboard with all squares
+    /// set that are attacked by White's bishops.
+    pub fn get_piece_attack_bb(&self, piece: Piece, color: Color) -> Bitboard {
+        // get a reference to the lookup table
+        let lookup = LOOKUP_TABLE.get().unwrap();
+        // the result attack_bb
+        let mut attack_bb = Bitboard::new(0);
+        // the bitboard for the given piece of the given color
+        let piece_bb = self.pieces[color.to_index() as usize][piece.to_index() as usize];
+        // get squares with pieces on them
+        let active_squares = piece_bb.get_active_bits();
+        // get blocker bitboard (needed for slider pieces)
+        let blockers = self.get_occupancies();
+
+        // loop over active squares and `or` the result attack_bb with the attack bitboard of the piece on each square
+        for square in active_squares {
+            match piece {
+                Piece::Pawn => attack_bb.value |= lookup.get_pawn_attacks(square, color).value,
+                Piece::Knight => attack_bb.value |= lookup.get_knight_attacks(square).value,
+                Piece::Bishop => attack_bb.value |= lookup.get_bishop_attacks(square, blockers).value,
+                Piece::Rook => attack_bb.value |= lookup.get_rook_attacks(square, blockers).value,
+                Piece::Queen => attack_bb.value |= lookup.get_queen_attacks(square, blockers).value,
+                Piece::King => attack_bb.value |= lookup.get_king_attacks(square).value,
+            };
+        }
+
+        attack_bb
+    }
 }
 
 /// Prints the position with '.' marking empty squares, capital letters marking white pieces,
@@ -122,9 +168,13 @@ mod tests {
     use crate::board::castling_rights::CastlingRights::NoRights;
     use crate::board::color::Color::{Black, White};
     use crate::board::{Board};
+    use crate::board::color::Color;
+    use crate::board::piece::Piece;
     use crate::board::piece::Piece::{Bishop, King, Knight, Pawn, Queen, Rook};
     use crate::board::position::Position;
     use crate::board::square::{A1, A3, E1, E4, F2, F3, G3, H7, H8};
+    use crate::lookup::LOOKUP_TABLE;
+    use crate::lookup::lookup_table::LookupTable;
 
     #[test]
     fn default_returns_position_with_default_values() {
@@ -227,5 +277,100 @@ mod tests {
         let position = Board::parse_fen("r1bq1rk1/1pp1bppp/p1n2n2/4p3/2PpP1P1/P2P1Q1P/1P1N1P2/R1B1KBNR b KQ g3 0 9").unwrap().position;
         let expected_output = "8  r  .  b  q  .  r  k  .  \n7  .  p  p  .  b  p  p  p  \n6  p  .  n  .  .  n  .  .  \n5  .  .  .  .  p  .  .  .  \n4  .  .  P  p  P  .  P  .  \n3  P  .  .  P  .  Q  .  P  \n2  .  P  .  N  .  P  .  .  \n1  R  .  B  .  K  B  N  R  \n   a  b  c  d  e  f  g  h\n\nMove: Black\nCastling: Both - NoRights\nEn Passant: g3\n";
         assert_eq!(expected_output, format!("{}", position));
+    }
+
+    #[test]
+    fn test_get_attack_bb() {
+        let mut lookup = LookupTable::default();
+        lookup.initialize_tables();
+        let _ = LOOKUP_TABLE.set(lookup);
+
+        // -----------------------------------------------------------------------------------------
+        // position 1 (starting position)
+        // -----------------------------------------------------------------------------------------
+
+        let position = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap().position;
+        let attack_bb = position.get_attack_bb(White);
+        assert_eq!(0xffff7e, attack_bb.value);
+
+        // -----------------------------------------------------------------------------------------
+        // position 2
+        // -----------------------------------------------------------------------------------------
+
+        let position = Board::from_fen("r1bq1rk1/p5pp/3p1p2/1ppP2b1/2Pp1B2/1P1P1B2/P2Q1PPP/R3R1K1 b - - 3 17").unwrap().position;
+        let attack_bb = position.get_attack_bb(White);
+        assert_eq!(0x10101cdb77feffff, attack_bb.value);
+
+        // -----------------------------------------------------------------------------------------
+        // position 3
+        // -----------------------------------------------------------------------------------------
+
+        let position = Board::from_fen("r3nrk1/2qn2pp/1p1bb3/1Q3p2/3P4/1N2P1N1/PP1BB1PP/R4RK1 w - - 2 21").unwrap().position;
+        let attack_bb = position.get_attack_bb(Black);
+        assert_eq!(0xfeffef3d77470504, attack_bb.value);
+    }
+
+    #[test]
+    fn test_get_piece_attack_bb() {
+        let mut lookup = LookupTable::default();
+        lookup.initialize_tables();
+        let _ = LOOKUP_TABLE.set(lookup);
+
+        // -----------------------------------------------------------------------------------------
+        // position 1 (starting position)
+        // -----------------------------------------------------------------------------------------
+
+        let position = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap().position;
+        let pawn_attack_bb = position.get_piece_attack_bb(Pawn, Black);
+        let knight_attack_bb = position.get_piece_attack_bb(Knight, Black);
+        let bishop_attack_bb = position.get_piece_attack_bb(Piece::Bishop, Color::Black);
+        let rook_attack_bb = position.get_piece_attack_bb(Piece::Rook, Color::Black);
+        let queen_attack_bb = position.get_piece_attack_bb(Piece::Queen, Color::Black);
+        let king_attack_bb = position.get_piece_attack_bb(Piece::King, Color::Black);
+
+        assert_eq!(0xff0000000000, pawn_attack_bb.value);
+        assert_eq!(6936818859638784, knight_attack_bb.value);
+        assert_eq!(0x5a000000000000, bishop_attack_bb.value);
+        assert_eq!(0x4281000000000000, rook_attack_bb.value);
+        assert_eq!(0x141c000000000000, queen_attack_bb.value);
+        assert_eq!(0x2838000000000000, king_attack_bb.value);
+
+        // -----------------------------------------------------------------------------------------
+        // position 2
+        // -----------------------------------------------------------------------------------------
+
+        let position = Board::from_fen("6k1/2p2pp1/7p/N7/8/1Pn3P1/4r1qP/R6K w - - 0 29").unwrap().position;
+        let pawn_attack_bb = position.get_piece_attack_bb(Piece::Pawn, Color::White);
+        let knight_attack_bb = position.get_piece_attack_bb(Piece::Knight, Color::White);
+        let bishop_attack_bb = position.get_piece_attack_bb(Piece::Bishop, Color::White);
+        let rook_attack_bb = position.get_piece_attack_bb(Piece::Rook, Color::White);
+        let queen_attack_bb = position.get_piece_attack_bb(Piece::Queen, Color::White);
+        let king_attack_bb = position.get_piece_attack_bb(Piece::King, Color::White);
+
+        assert_eq!(0xa5400000, pawn_attack_bb.value);
+        assert_eq!(0x2040004020000, knight_attack_bb.value);
+        assert_eq!(0, bishop_attack_bb.value);
+        assert_eq!(0x1010101fe, rook_attack_bb.value);
+        assert_eq!(0, queen_attack_bb.value);
+        assert_eq!(0xc040, king_attack_bb.value);
+
+        // -----------------------------------------------------------------------------------------
+        // position 3
+        // -----------------------------------------------------------------------------------------
+
+        let position = Board::from_fen("r7/pb3k2/1p4p1/3N1p2/2P2Np1/3BR3/PP4PP/6K1 b - - 1 28").unwrap().position;
+        let pawn_attack_bb = position.get_piece_attack_bb(Piece::Pawn, Color::White);
+        let knight_attack_bb = position.get_piece_attack_bb(Piece::Knight, Color::White);
+        let bishop_attack_bb = position.get_piece_attack_bb(Piece::Bishop, Color::White);
+        let rook_attack_bb = position.get_piece_attack_bb(Piece::Rook, Color::White);
+        let queen_attack_bb = position.get_piece_attack_bb(Piece::Queen, Color::White);
+        let king_attack_bb = position.get_piece_attack_bb(Piece::King, Color::White);
+
+        assert_eq!(0xa00e70000, pawn_attack_bb.value);
+        assert_eq!(0x147288229c5000, knight_attack_bb.value);
+        assert_eq!(0x2014001422, bishop_attack_bb.value);
+        assert_eq!(0x1010101010e81010, rook_attack_bb.value);
+        assert_eq!(0, queen_attack_bb.value);
+        assert_eq!(0xe0a0, king_attack_bb.value);
     }
 }
--- src/move_gen.rs
+++ src/move_gen.rs
@@ -1,9 +1,4 @@
 //! The move_gen module is responsible for the move generation logic.
-
-use crate::board::bitboard::Bitboard;
-use crate::board::color::Color;
-use crate::board::piece::{NUM_PIECES, Piece};
-use crate::board::position::Position;
 use crate::lookup::lookup_table::LookupTable;
 
 pub mod ply;
@@ -11,160 +6,16 @@ mod pawn_moves;
 
 /// This is the move generator. It can generate all legal moves (ply) for a given position.
 /// For the move generator to work, it has to receive an initialized lookup table.
-struct MoveGenerator {
+pub struct MoveGenerator {
     lookup: LookupTable,
 }
 
 impl MoveGenerator {
     /// Constructs a new move generator.
-    fn new(lookup_table: LookupTable) -> Self {
+    pub fn new(lookup_table: LookupTable) -> Self {
         MoveGenerator { lookup: lookup_table }
-    }
-
-    /// Returns the attack bitboard for all pieces of the given color combined.
-    /// 
-    /// For example `get_attack_bb_combined(position, Color::White)` will return a bitboard with all squares
-    /// set that are attacked by any of White's pieces.
-    fn get_attack_bb_combined(&self, position: Position, color: Color) -> Bitboard {
-        // the result attack_bb
-        let mut attack_bb = Bitboard::new(0);
-        // `or` the attack bitboards for all pieces of the given color
-        for piece_index in 0..NUM_PIECES {
-            attack_bb.value |= self.get_attack_bb(position, Piece::from_index(piece_index), color).value;
-        }
-        attack_bb
-    }
-
-    /// Returns the attack bitboard for a given type of piece of the given color for the given position.
-    ///
-    /// For example `get_attack_bb(position, Piece::Bishop, Color::White)` will return a bitboard with all squares
-    /// set that are attacked by White's bishops.
-    fn get_attack_bb(&self, position: Position, piece: Piece, color: Color) -> Bitboard {
-        // the result attack_bb
-        let mut attack_bb = Bitboard::new(0);
-        // the bitboard for the given piece of the given color
-        let piece_bb = position.pieces[color.to_index() as usize][piece.to_index() as usize];
-        // get squares with pieces on them
-        let active_squares = piece_bb.get_active_bits();
-        // get blocker bitboard (needed for slider pieces)
-        let blockers = position.get_occupancies();
-
-        // loop over active squares and `or` the result attack_bb with the attack bitboard of the piece on each square
-        for square in active_squares {
-            match piece {
-                Piece::Pawn => attack_bb.value |= self.lookup.get_pawn_attacks(square, color).value,
-                Piece::Knight => attack_bb.value |= self.lookup.get_knight_attacks(square).value,
-                Piece::Bishop => attack_bb.value |= self.lookup.get_bishop_attacks(square, blockers).value,
-                Piece::Rook => attack_bb.value |= self.lookup.get_rook_attacks(square, blockers).value,
-                Piece::Queen => attack_bb.value |= self.lookup.get_queen_attacks(square, blockers).value,
-                Piece::King => attack_bb.value |= self.lookup.get_king_attacks(square).value,
-            };
-        }
-
-        attack_bb
     }
 }
 
 #[cfg(test)]
-mod tests {
-    use crate::board::Board;
-    use crate::board::color::Color;
-    use crate::board::piece::Piece;
-    use crate::lookup::lookup_table::LookupTable;
-    use crate::move_gen::MoveGenerator;
-
-    #[test]
-    fn test_get_combined_attack_bb() {
-        let mut lookup = LookupTable::default();
-        lookup.initialize_tables();
-        let generator = MoveGenerator::new(lookup);
-        
-        // -----------------------------------------------------------------------------------------
-        // position 1 (starting position)
-        // -----------------------------------------------------------------------------------------
-
-        let position = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap().position;
-        let attack_bb = generator.get_attack_bb_combined(position, Color::White);
-        assert_eq!(0xffff7e, attack_bb.value);
-
-        // -----------------------------------------------------------------------------------------
-        // position 2
-        // -----------------------------------------------------------------------------------------
-
-        let position = Board::from_fen("r1bq1rk1/p5pp/3p1p2/1ppP2b1/2Pp1B2/1P1P1B2/P2Q1PPP/R3R1K1 b - - 3 17").unwrap().position;
-        let attack_bb = generator.get_attack_bb_combined(position, Color::White);
-        assert_eq!(0x10101cdb77feffff, attack_bb.value);
-
-        // -----------------------------------------------------------------------------------------
-        // position 3
-        // -----------------------------------------------------------------------------------------
-
-        let position = Board::from_fen("r3nrk1/2qn2pp/1p1bb3/1Q3p2/3P4/1N2P1N1/PP1BB1PP/R4RK1 w - - 2 21").unwrap().position;
-        let attack_bb = generator.get_attack_bb_combined(position, Color::Black);
-        assert_eq!(0xfeffef3d77470504, attack_bb.value);
-    }
-    
-    #[test]
-    fn test_get_attack_bb() {
-        let mut lookup = LookupTable::default();
-        lookup.initialize_tables();
-        let generator = MoveGenerator::new(lookup);
-
-        // -----------------------------------------------------------------------------------------
-        // position 1 (starting position)
-        // -----------------------------------------------------------------------------------------
-
-        let position = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap().position;
-        let pawn_attack_bb = generator.get_attack_bb(position, Piece::Pawn, Color::Black);
-        let knight_attack_bb = generator.get_attack_bb(position, Piece::Knight, Color::Black);
-        let bishop_attack_bb = generator.get_attack_bb(position, Piece::Bishop, Color::Black);
-        let rook_attack_bb = generator.get_attack_bb(position, Piece::Rook, Color::Black);
-        let queen_attack_bb = generator.get_attack_bb(position, Piece::Queen, Color::Black);
-        let king_attack_bb = generator.get_attack_bb(position, Piece::King, Color::Black);
-
-        assert_eq!(0xff0000000000, pawn_attack_bb.value);
-        assert_eq!(6936818859638784, knight_attack_bb.value);
-        assert_eq!(0x5a000000000000, bishop_attack_bb.value);
-        assert_eq!(0x4281000000000000, rook_attack_bb.value);
-        assert_eq!(0x141c000000000000, queen_attack_bb.value);
-        assert_eq!(0x2838000000000000, king_attack_bb.value);
-
-        // -----------------------------------------------------------------------------------------
-        // position 2
-        // -----------------------------------------------------------------------------------------
-
-        let position = Board::from_fen("6k1/2p2pp1/7p/N7/8/1Pn3P1/4r1qP/R6K w - - 0 29").unwrap().position;
-        let pawn_attack_bb = generator.get_attack_bb(position, Piece::Pawn, Color::White);
-        let knight_attack_bb = generator.get_attack_bb(position, Piece::Knight, Color::White);
-        let bishop_attack_bb = generator.get_attack_bb(position, Piece::Bishop, Color::White);
-        let rook_attack_bb = generator.get_attack_bb(position, Piece::Rook, Color::White);
-        let queen_attack_bb = generator.get_attack_bb(position, Piece::Queen, Color::White);
-        let king_attack_bb = generator.get_attack_bb(position, Piece::King, Color::White);
-
-        assert_eq!(0xa5400000, pawn_attack_bb.value);
-        assert_eq!(0x2040004020000, knight_attack_bb.value);
-        assert_eq!(0, bishop_attack_bb.value);
-        assert_eq!(0x1010101fe, rook_attack_bb.value);
-        assert_eq!(0, queen_attack_bb.value);
-        assert_eq!(0xc040, king_attack_bb.value);
-
-        // -----------------------------------------------------------------------------------------
-        // position 3
-        // -----------------------------------------------------------------------------------------
-
-        let position = Board::from_fen("r7/pb3k2/1p4p1/3N1p2/2P2Np1/3BR3/PP4PP/6K1 b - - 1 28").unwrap().position;
-        let pawn_attack_bb = generator.get_attack_bb(position, Piece::Pawn, Color::White);
-        let knight_attack_bb = generator.get_attack_bb(position, Piece::Knight, Color::White);
-        let bishop_attack_bb = generator.get_attack_bb(position, Piece::Bishop, Color::White);
-        let rook_attack_bb = generator.get_attack_bb(position, Piece::Rook, Color::White);
-        let queen_attack_bb = generator.get_attack_bb(position, Piece::Queen, Color::White);
-        let king_attack_bb = generator.get_attack_bb(position, Piece::King, Color::White);
-
-        assert_eq!(0xa00e70000, pawn_attack_bb.value);
-        assert_eq!(0x147288229c5000, knight_attack_bb.value);
-        assert_eq!(0x2014001422, bishop_attack_bb.value);
-        assert_eq!(0x1010101010e81010, rook_attack_bb.value);
-        assert_eq!(0, queen_attack_bb.value);
-        assert_eq!(0xe0a0, king_attack_bb.value);
-    }
-}
+mod tests {}
