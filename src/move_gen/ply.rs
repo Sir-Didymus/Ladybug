445dcffa925cb4d20a7665aa826dad37bbf1cf39
cs-patzer <cs-patzer@protonmail.com>
2024-04-06 16:19:23 +0200

    Implement Display for Ply

--- src/move_gen/ply.rs
+++ src/move_gen/ply.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use crate::board::piece::Piece;
 use crate::board::square::Square;
 
@@ -18,4 +19,41 @@ pub struct Ply {
     pub captured_piece: Option<Piece>,
     /// If the move is a pawn promotion, this field will contain the promotion piece.
     pub promotion_piece: Option<Piece>,
+}
+
+/// Prints the ply as text.
+impl Display for Ply {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        let mut output: String = String::from("");
+        output += format!("{}", self.source).as_str();
+        output += format!("{}", self.target).as_str();
+        write!(f, "{}" ,output)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::board::piece::Piece;
+    use crate::board::square;
+    use crate::move_gen::ply::Ply;
+
+    #[test]
+    fn ply_formats_correctly() {
+        let mut ply = Ply {
+            source: square::E2,
+            target: square::E4,
+            piece: Piece::Pawn,
+            captured_piece: None,
+            promotion_piece: None,
+        };
+        assert_eq!("e2e4", format!("{ply}"));
+        
+        ply.source = square::A4;
+        ply.target = square::E8;
+        assert_eq!("a4e8", format!("{ply}"));
+
+        ply.source = square::H3;
+        ply.target = square::H6;
+        assert_eq!("h3h6", format!("{ply}"));
+    }
 }
