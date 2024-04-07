33aa8a7fc1b4005eb615eee76a2f9a6b64c38319
cs-patzer <cs-patzer@protonmail.com>
2024-04-07 13:45:48 +0200

    Test queen move generation and fix bug where I generated rook moves instead of queen moves

--- src/move_gen/slider_moves.rs
+++ src/move_gen/slider_moves.rs
@@ -31,7 +31,7 @@ fn generate_slider_moves_by_piece(position: Position, piece: Piece) -> Vec<Ply> 
         let mut attack_bb = match piece {
             Piece::Bishop => lookup.get_bishop_attacks(source_square, occupancies),
             Piece::Rook => lookup.get_rook_attacks(source_square, occupancies),
-            Piece::Queen => lookup.get_rook_attacks(source_square, occupancies),
+            Piece::Queen => lookup.get_queen_attacks(source_square, occupancies),
             _non_slider => return move_list
         };
 
@@ -207,5 +207,72 @@ mod tests {
         let position = Board::from_fen("4R2b/8/5R2/r1R1K3/3R1R2/2b5/5r1b/7k w - - 0 1").unwrap().position;
         let move_list = generate_slider_moves_by_piece(position, Piece::Rook);
         assert_eq!(12, move_list.len());
+    }
+
+    #[test]
+    fn test_generate_slider_moves_by_piece_for_queen() {
+        let mut lookup = LookupTable::default();
+        lookup.initialize_tables();
+        let _ = LOOKUP_TABLE.set(lookup);
+
+        // position 1 (starting position)
+
+        let position = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap().position;
+        let move_list = generate_slider_moves_by_piece(position, Piece::Queen);
+        assert_eq!(0, move_list.len());
+
+        // position 2
+
+        let position = Board::from_fen("rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2").unwrap().position;
+        let move_list = generate_slider_moves_by_piece(position, Piece::Queen);
+        assert_eq!(4, move_list.len());
+
+        // position 3
+
+        let position = Board::from_fen("1nbqkbnr/r2ppppp/p1p5/1p5Q/2B1P3/8/PPPP1PPP/RNB1K1NR w KQk - 2 5").unwrap().position;
+        let move_list = generate_slider_moves_by_piece(position, Piece::Queen);
+        assert_eq!(16, move_list.len());
+
+        // position 4
+
+        let position = Board::from_fen("1nb1kbnr/r2pqQpp/p1p1p3/1p6/2B1P3/P4P2/1PPP2PP/RNB1K1NR b KQk - 0 7").unwrap().position;
+        let move_list = generate_slider_moves_by_piece(position, Piece::Queen);
+        assert_eq!(1, move_list.len());
+
+        // position 5
+
+        let position = Board::from_fen("1nb1kbnr/r4qpp/p1ppp3/1p6/2B1P2P/P4P2/1PPP2P1/RNB1K1NR b KQk - 0 9").unwrap().position;
+        let move_list = generate_slider_moves_by_piece(position, Piece::Queen);
+        assert_eq!(10, move_list.len());
+
+        // position 6
+
+        let position = Board::from_fen("1nb1kbn1/B4qpr/p1ppp3/1p5p/2B1P2P/P2P1P2/1PP3P1/RN2K1NR b KQ - 0 12").unwrap().position;
+        let move_list = generate_slider_moves_by_piece(position, Piece::Queen);
+        assert_eq!(10, move_list.len());
+
+        // position 7
+
+        let position = Board::from_fen("r1bqkb1r/pppp1Qpp/2n2n2/6N1/4P3/8/PPP2PPP/RNB1K2R b KQ - 4 9").unwrap().position;
+        let move_list = generate_slider_moves_by_piece(position, Piece::Queen);
+        assert_eq!(0, move_list.len());
+
+        // position 8
+
+        let position = Board::from_fen("r1bqr1k1/ppp2ppp/2nb1n2/8/3pP2Q/2NB1P2/PPPB2PP/2KR2NR w - - 0 12").unwrap().position;
+        let move_list = generate_slider_moves_by_piece(position, Piece::Queen);
+        assert_eq!(11, move_list.len());
+
+        // position 9
+
+        let position = Board::from_fen("r2q1rk1/pp2bppp/3pn3/2pN4/4PPb1/1PPBQ2P/P5P1/R1B2RK1 b - - 0 17").unwrap().position;
+        let move_list = generate_slider_moves_by_piece(position, Piece::Queen);
+        assert_eq!(7, move_list.len());
+
+        // position 10
+
+        let position = Board::from_fen("r2qk1nr/ppp2p1p/7b/1b1QP3/3n1B1P/8/PPP3P1/RN3RK1 w kq - 0 12").unwrap().position;
+        let move_list = generate_slider_moves_by_piece(position, Piece::Queen);
+        assert_eq!(14, move_list.len());
     }
 }
