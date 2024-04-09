e099051711d39ab9b3c75565c9b3ea1cec237dd7
cs-patzer <cs-patzer@protonmail.com>
2024-04-09 12:12:05 +0200

    Add read_input and write_output functions

--- src/ladybug.rs
+++ src/ladybug.rs
@@ -1,3 +1,6 @@
+use std::sync::mpsc::Receiver;
+use crate::uci::{parse_uci, UciCommand};
+
 /// The main character in this project!
 /// The Ladybug struct acts as the UCI client and can receive and handle UCI commands.
 pub struct Ladybug {
@@ -10,8 +13,30 @@ impl Default for Ladybug {
     fn default() -> Self {
         Self {
             name: String::from("Ladybug 0.1.0"),
-            author: String::from("\"cs-patzer\" - Felix O."),
+            author: String::from("Felix O."),
         }
+    }
+}
+
+impl Ladybug {
+    /// Start running Ladybug.
+    pub fn run(&self, receiver: Receiver<String>) {
+        loop {
+            let mut input = String::new();
+            
+            input = receiver.recv().unwrap();
+            
+            let uci_command = parse_uci(input).unwrap();
+            
+            match uci_command {
+                UciCommand::Uci => self.handle_uci(),
+            }
+        }
+    }
+    
+    fn handle_uci(&self) {
+        println!("id name {}", self.name);
+        println!("id author {}", self.author);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::{io, thread};
 use ladybug::ladybug::Ladybug;
 use ladybug::lookup::LOOKUP_TABLE;
 use ladybug::lookup::lookup_table::LookupTable;
 
 fn main() {
     println!("\nLadybug 0.1.0\n");
-    
+
     print!("Initializing tables... ");
-    
+
     // initialize the lookup table
     let mut lookup = LookupTable::default();
     lookup.initialize_tables();
@@ -15,6 +18,38 @@ fn main() {
     println!("Done!");
 
     println!("Type \"help\" to see a list of all commands.\n");
+
+    // create input_sender and input_receiver so that the input thread can send input to the ladybug thread
+    let (input_sender, input_receiver) : (Sender<String>, Receiver<String>) = mpsc::channel();
+
+    // create output_sender and output_receiver so that the ladybug thread can send output to the output thread.
+    let (output_sender, output_receiver) : (Sender<String>, Receiver<String>) = mpsc::channel();
     
+    // spawn the input thread
+    thread::spawn(move || read_input(input_sender));
+
+    // spawn the output thread
+    thread::spawn(move || write_output(output_receiver));
+
     let ladybug = Ladybug::default();
+    
+    ladybug.run(input_receiver);
+}
+
+/// Reads input from Stdin and sends it to Ladybug.
+pub fn read_input(sender: Sender<String>) {
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        sender.send(input).unwrap()
+    }
+}
+
+/// Receives output from Ladybug and writes it to Stdout.
+pub fn write_output(receiver: Receiver<String>) {
+    loop {
+        let mut output = String::new();
+        output = receiver.recv().unwrap();
+        println!("{}", output);
+    }
 }
