4a31cc4b52ff92ceec1e1000b7fa9698cc32fb54
cs-patzer <cs-patzer@protonmail.com>
2024-04-10 19:45:22 +0200

    Add UCI Position command

--- src/uci.rs
+++ src/uci.rs
@@ -3,6 +3,7 @@
 pub enum UciCommand {
     Uci,
     IsReady,
+    Position(Vec<String>),
     Quit,
     Help
 }
@@ -10,7 +11,7 @@ pub enum UciCommand {
 /// Tries to parse a string as a UCI command.
 pub fn parse_uci(input: String) -> Result<UciCommand, String> {
     // split input string into parts
-    let uci_parts: Vec<String> = input.split_whitespace().map(|s| s.to_string()).collect();
+    let mut uci_parts: Vec<String> = input.split_whitespace().map(|s| s.to_string()).collect();
     
     // if uci_parts is empty, return error
     if uci_parts.is_empty() {
@@ -21,6 +22,12 @@ pub fn parse_uci(input: String) -> Result<UciCommand, String> {
     match uci_parts[0].as_str() {
         "uci" => Ok(UciCommand::Uci),
         "isready" => Ok(UciCommand::IsReady),
+        "position" => {
+            match uci_parts.len() > 1 {
+                false => Err(String::from("info string unknown command")),
+                true => Ok(UciCommand::Position(uci_parts.split_off(1)))
+            }
+        }
         "quit" => Ok(UciCommand::Quit),
         "help" => Ok(UciCommand::Help),
         _other => Err(String::from("info string unknown command")),
@@ -47,6 +54,19 @@ mod tests {
     #[test]
     fn test_parse_uci_for_isready() {
         assert_eq!(UciCommand::IsReady, uci::parse_uci(String::from("isready")).unwrap());
+    }
+
+    #[test]
+    fn test_parse_uci_for_position() {
+        assert_eq!(Err(String::from("info string unknown command")), uci::parse_uci(String::from("position")));
+        
+        assert_eq!(Ok(UciCommand::Position(vec!(String::from("startpos"), String::from("moves"), String::from("h3h4"), String::from("c6g2")))),
+                   uci::parse_uci(String::from("position startpos moves h3h4 c6g2")));
+
+        assert_eq!(Ok(UciCommand::Position(vec!(String::from("fen"), String::from("8/B6p/2b1k1p1/5p2/2PK4/6PP/6P1/8"), String::from("w"), String::from("-"),
+                                                String::from("-"), String::from("1"), String::from("45"),
+                                                 String::from("moves"), String::from("h3h4"), String::from("c6g2")))),
+                   uci::parse_uci(String::from("position fen 8/B6p/2b1k1p1/5p2/2PK4/6PP/6P1/8 w - - 1 45 moves h3h4 c6g2")));
     }
 
     #[test]
