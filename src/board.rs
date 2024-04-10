ac4fb4c88fafc806d1089ae72036d9efe355c624
cs-patzer <cs-patzer@protonmail.com>
2024-04-10 23:10:20 +0200

    Add make_move function to board.rs

--- src/board.rs
+++ src/board.rs
@@ -3,6 +3,9 @@
 //! This module is the foundation on which the rest of the engine builds upon.
 
 use position::Position;
+use crate::board::color::Color;
+use crate::board::piece::Piece;
+use crate::move_gen::ply::Ply;
 
 pub mod bitboard;
 pub mod color;
@@ -46,16 +49,40 @@ impl Board {
     pub fn from_fen(fen: &str) -> Result<Board, String> {
         Self::parse_fen(fen)
     }
+
+    /// Returns a new Board that reflects the board state where the given move (ply) has been played.
+    pub fn make_move(&self, ply: Ply) -> Board {
+        let mut board = *self;
+        
+        board.position = board.position.make_move(ply);
+        
+        if ply.piece != Piece::Pawn && ply.captured_piece.is_none() {
+            // if the move is neither a pawn move nor a capture, increment the halfmove clock
+            board.halfmove_clock += 1;
+        } else {
+            // otherwise, reset it
+            board.halfmove_clock = 0;
+        }
+        
+        if self.position.color_to_move == Color::Black {
+            board.fullmove_counter += 1;
+        }
+        
+        board
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::board::bitboard::Bitboard;
-    use crate::board::Board;
+    use crate::board::{Board, square};
     use crate::board::castling_rights::CastlingRights;
     use crate::board::color::Color::{Black, White};
+    use crate::board::piece::Piece;
     use crate::board::position::Position;
+    use crate::lookup::LOOKUP_TABLE;
     use crate::lookup::lookup_table::LookupTable;
+    use crate::move_gen::ply::Ply;
 
     #[test]
     fn default_returns_board_with_default_values() {
@@ -120,5 +147,103 @@ mod tests {
         assert_eq!(None, board.position.en_passant);
         assert_eq!(4, board.halfmove_clock);
         assert_eq!(29, board.fullmove_counter);
+    }
+    
+    #[test]
+    fn test_make_move() {
+        let mut lookup = LookupTable::default();
+        lookup.initialize_tables();
+        let _ = LOOKUP_TABLE.set(lookup);
+        
+        // g1-f3
+        let board = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap();
+        let board = board.make_move(Ply {
+            source: square::G1,
+            target: square::F3,
+            piece: Piece::Knight,
+            captured_piece: None,
+            promotion_piece: None,
+        });
+        assert_eq!(Board::from_fen("rnbqkbnr/pppppppp/8/8/8/5N2/PPPPPPPP/RNBQKB1R b KQkq - 1 1").unwrap(), board);
+
+        // b8-c6
+        let board = board.make_move(Ply {
+            source: square::B8,
+            target: square::C6,
+            piece: Piece::Knight,
+            captured_piece: None,
+            promotion_piece: None,
+        });
+        assert_eq!(Board::from_fen("r1bqkbnr/pppppppp/2n5/8/8/5N2/PPPPPPPP/RNBQKB1R w KQkq - 2 2").unwrap(), board);
+
+        // b1-c3
+        let board = board.make_move(Ply {
+            source: square::B1,
+            target: square::C3,
+            piece: Piece::Knight,
+            captured_piece: None,
+            promotion_piece: None,
+        });
+        assert_eq!(Board::from_fen("r1bqkbnr/pppppppp/2n5/8/8/2N2N2/PPPPPPPP/R1BQKB1R b KQkq - 3 2").unwrap(), board);
+
+        // g8-f6
+        let board = board.make_move(Ply {
+            source: square::G8,
+            target: square::F6,
+            piece: Piece::Knight,
+            captured_piece: None,
+            promotion_piece: None,
+        });
+        assert_eq!(Board::from_fen("r1bqkb1r/pppppppp/2n2n2/8/8/2N2N2/PPPPPPPP/R1BQKB1R w KQkq - 4 3").unwrap(), board);
+
+        // f3-e5
+        let board = board.make_move(Ply {
+            source: square::F3,
+            target: square::E5,
+            piece: Piece::Knight,
+            captured_piece: None,
+            promotion_piece: None,
+        });
+        assert_eq!(Board::from_fen("r1bqkb1r/pppppppp/2n2n2/4N3/8/2N5/PPPPPPPP/R1BQKB1R b KQkq - 5 3").unwrap(), board);
+
+        // c6-e5
+        let board = board.make_move(Ply {
+            source: square::C6,
+            target: square::E5,
+            piece: Piece::Knight,
+            captured_piece: Some(Piece::Knight),
+            promotion_piece: None,
+        });
+        assert_eq!(Board::from_fen("r1bqkb1r/pppppppp/5n2/4n3/8/2N5/PPPPPPPP/R1BQKB1R w KQkq - 0 4").unwrap(), board);
+
+        // d2-d4
+        let board = board.make_move(Ply {
+            source: square::D2,
+            target: square::D4,
+            piece: Piece::Pawn,
+            captured_piece: None,
+            promotion_piece: None,
+        });
+        assert_eq!(Board::from_fen("r1bqkb1r/pppppppp/5n2/4n3/3P4/2N5/PPP1PPPP/R1BQKB1R b KQkq d3 0 4").unwrap(), board);
+
+        // e5-c6
+        let board = board.make_move(Ply {
+            source: square::E5,
+            target: square::C6,
+            piece: Piece::Knight,
+            captured_piece: None,
+            promotion_piece: None,
+        });
+        assert_eq!(Board::from_fen("r1bqkb1r/pppppppp/2n2n2/8/3P4/2N5/PPP1PPPP/R1BQKB1R w KQkq - 1 5").unwrap(), board);
+
+        // e5-c6
+        let board = board.make_move(Ply {
+            source: square::D4,
+            target: square::D5,
+            piece: Piece::Pawn,
+            captured_piece: None,
+            promotion_piece: None,
+        });
+        assert_eq!(Board::from_fen("r1bqkb1r/pppppppp/2n2n2/3P4/8/2N5/PPP1PPPP/R1BQKB1R b KQkq - 0 5").unwrap(), board);
     }
 }
