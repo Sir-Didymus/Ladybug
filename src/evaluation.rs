1708962f0da196a316ddbbe599cc55bc857bafb1
cs-patzer <cs-patzer@protonmail.com>
2024-04-14 16:20:49 +0200

    Add piece square tables

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -1,9 +1,8 @@
-use crate::board::color::Color;
+use crate::board::color::{Color, NUM_COLORS};
 use crate::board::piece::{NUM_PIECES, Piece};
 use crate::board::position::Position;
 
-/// The values for each piece type.
-pub const PIECE_VALUES: [i32; 6] = [100, 300, 300, 500, 900, POSITIVE_INFINITY];
+mod pst;
 /// The highest possible value.
 pub const POSITIVE_INFINITY: i32 = i32::MAX - 1;
 /// The lowest possible value.
@@ -13,7 +12,7 @@ pub const NEGATIVE_INFINITY: i32 = i32::MIN + 1;
 /// Returns the static evaluation for the given position.
 ///
 /// The evaluation is always done from the point of view of the side whose turn it is.
-/// E.g. if it is Black's turn, and black is up a queen, the evaluation will return +9, 
+/// E.g. if it is Black's turn, and black is up a queen, the evaluation will return +900, 
 /// even though chess players usually refer to such a position, from White's point of view, as -9.
 pub fn evaluate(position: Position) -> i32 {
     evaluate_material(position)
@@ -22,9 +21,16 @@ pub fn evaluate(position: Position) -> i32 {
 /// Returns the purely materialistic evaluation of the position.
 fn evaluate_material(position: Position) -> i32 {
     let mut material_score: i32 = 0;
-    for piece_index in 0..NUM_PIECES {
-        material_score += (position.get_num_pieces(Piece::from_index(piece_index), Color::White) as i32 
-            - position.get_num_pieces(Piece::from_index(piece_index), Color::Black) as i32) * PIECE_VALUES[piece_index as usize];
+    for color_index in 0..NUM_COLORS {
+        for piece_index in 0..NUM_PIECES {
+            let active_bits = position.pieces[color_index as usize][piece_index as usize].get_active_bits();
+            for square in active_bits {
+                match Color::from_index(color_index) {
+                    Color::White => material_score += pst::get_piece_value(Piece::from_index(piece_index), square, Color::from_index(color_index)),
+                    Color::Black => material_score -= pst::get_piece_value(Piece::from_index(piece_index), square, Color::from_index(color_index)),
+                }
+            }
+        }
     }
     // if it is Black's move, negate the material score so that the evaluation is from Black's perspective
     if position.color_to_move == Color::Black {
@@ -45,24 +51,24 @@ mod tests {
         let mut lookup = LookupTable::default();
         lookup.initialize_tables();
         let _ = LOOKUP_TABLE.set(lookup);
-        
+
         let position = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap().position;
         assert_eq!(0, evaluate_material(position));
 
         // White is missing a queen - White to move
         let position = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNB1KBNR w KQkq - 0 1").unwrap().position;
-        assert_eq!(-900, evaluate_material(position));
+        assert!(evaluate_material(position) < -800);
 
         // White is missing a queen - Black to move
         let position = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNB1KBNR b KQkq - 0 1").unwrap().position;
-        assert_eq!(900, evaluate_material(position));
+        assert!(evaluate_material(position) > 800);
 
         // Black is missing a knight - White to move
         let position = Board::from_fen("rnbqkb1r/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1").unwrap().position;
-        assert_eq!(300, evaluate_material(position));
+        assert!(evaluate_material(position) > 200);
 
         // Black is missing a knight - Black to move
         let position = Board::from_fen("rnbqkb1r/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1").unwrap().position;
-        assert_eq!(-300, evaluate_material(position));
+        assert!(evaluate_material(position) < -200);
     }
 }
---
+++ src/evaluation/pst.rs
@@ -0,0 +1,114 @@
+//! This module contains the piece-square-tables.
+//!
+//! [PSTs](https://www.chessprogramming.org/Piece-Square_Tables) are used to give a piece's value a 
+//! bonus or penalty depending on which square the piece is located at.
+//! This is necessary to encourage the engine to place its piece's on positionally advantageous squares.
+//! Otherwise, it would just shuffle pieces around aimlessly.
+//!
+//! The values here are directly taken from the [Simplified Evaluation Function](https://www.chessprogramming.org/Simplified_Evaluation_Function#See_also).
+
+
+use crate::board::color::Color;
+use crate::board::piece::Piece;
+use crate::board::square::Square;
+
+/// Returns the relative value of the given piece, based on its color and square.
+pub fn get_piece_value(piece: Piece, square: Square, color: Color) -> i32 {
+    let square_index = match color {
+        Color::White => square.index ^ 56,
+        Color::Black => square.index,
+    };
+    PIECE_VALUES[piece.to_index() as usize] + PST[piece.to_index() as usize][square_index as usize]
+}
+
+/// The PST for all pieces.
+const PST: [[i32; 64]; 6] = [
+    PAWN_VALUES,
+    KNIGHT_VALUES,
+    BISHOP_VALUES,
+    ROOK_VALUES,
+    QUEEN_VALUES,
+    KING_VALUES,
+];
+
+/// The base values of the pieces.
+const PIECE_VALUES: [i32; 6] = [
+    100,
+    320,
+    330,
+    500,
+    950,
+    20000,
+];
+
+/// PST for pawns.
+const PAWN_VALUES: [i32; 64] = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5, -5,-10,  0,  0,-10, -5,  5,
+    5, 10, 10,-20,-20, 10, 10,  5,
+    0,  0,  0,  0,  0,  0,  0,  0
+];
+
+/// PST for knights.
+const KNIGHT_VALUES: [i32; 64] = [
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50,
+];
+
+/// PST for bishops.
+const BISHOP_VALUES: [i32; 64] = [
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20,
+];
+
+/// PST for rooks.
+const ROOK_VALUES: [i32; 64] = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10, 10, 10, 10, 10,  5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    0,  0,  0,  5,  5,  0,  0,  0
+];
+
+/// PST for queens.
+const QUEEN_VALUES: [i32; 64] = [
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+    -5,  0,  5,  5,  5,  5,  0, -5,
+    0,  0,  5,  5,  5,  5,  0, -5,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20
+];
+
+/// PST for kings.
+const KING_VALUES: [i32; 64] = [
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -20,-30,-30,-40,-40,-30,-30,-20,
+    -10,-20,-20,-20,-20,-20,-20,-10,
+    20, 20,  0,  0,  0,  0, 20, 20,
+    20, 30, 10,  0,  0, 10, 30, 20
+];
